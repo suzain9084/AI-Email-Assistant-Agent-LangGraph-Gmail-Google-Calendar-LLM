f784ce3b3102f4b3097d95627268ded6573768f2
suzain9084 <suzain@ce.du.ac.in>
2026-08-04 17:36:19 +0530

    improve working of final workflow

--- nodes/send_email_node.py
+++ nodes/send_email_node.py
@@ -1,28 +1,66 @@
-from state import EmailAgentState
-from tools.gmail_tools import send_gmail_message
-from langchain_core.messages import AIMessage
+from dotenv import load_dotenv
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.prebuilt import create_react_agent
+from langchain_core.prompts import ChatPromptTemplate
+
+from state import EmailAgentState
+from tools.calendar_tools import CALENDAR_TOOLS
+from tools.gmail_tools import EMAIL_TOOLS
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7,
+    prompt="You are an email assistant. If the email requires any calendar action, perform it before sending the email."
+)
+
+tools = [
+    *CALENDAR_TOOLS,
+    *EMAIL_TOOLS,
+]
+
+agent = create_react_agent(
+    model=llm,
+    tools=tools
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        HumanMessage(content="""
+            To: {to}
+            Subject: {subject}
+            CC: {cc}
+            BCC: {bcc}
+
+            Body:
+            {message}
+        """)
+    ]
+)
 
 def email_send_node(state: EmailAgentState):
     reply = state["reply"]
-
-    email = f"""
-        To: {reply.to}
-        Subject: {reply.subject}
-        CC: {", ".join(reply.cc)}
-        BCC: {", ".join(reply.bcc)}
-
-        {reply.message}
-    """
+    chain = prompt | agent 
 
     try:
-        result = send_gmail_message.invoke(
-            {
-                "message": email,
-            }
-        )
+        result = chain.invoke({
+            "to": reply["to"],
+            "subject": reply["subject"],
+            "cc": reply["cc"],
+            "bcc": reply["bcc"],
+            "message": reply["message"],
+        })
 
-        return {"gmail_response": result}
+        final_response = result["messages"][-1].content
+
+        return {
+            "gmail_response": final_response
+        }
 
     except Exception as e:
-        return {"gmail_response": str(e)}
+        return {
+            "gmail_response": str(e)
+        }
